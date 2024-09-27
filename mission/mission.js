a1b029d766050636d027d03075ef179c1ada0695
lyssahenderson <hen20002@byui.edu>
2024-09-27 12:06:56 -0600

    mission

--- mission/mission.js
+++ mission/mission.js
@@ -1,14 +1,16 @@
 function setTheme() {
     const themeValue = document.querySelector("select").value;
-    console.log("Theme" + themeValue);
-    
-    if (themeValue === "Dark"){
+    console.log("Theme: " + themeValue);
+
+    if (themeValue === "Dark") {
         document.body.classList.add("dark");
         document.querySelector("img").setAttribute("src", "./images/byui-logo_white.png");
-    }
-    else {
+    } else {
         document.body.classList.remove("dark");
         document.querySelector("img").setAttribute("src", "./images/logo.webp");
     }
 }
-document.querySelector("select").addEventListener("change", setTheme);
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector("select").addEventListener("change", setTheme);
+});
